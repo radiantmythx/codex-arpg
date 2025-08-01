91a142b79993baee11bab25b7688a38cfa074c1b
radiantmythx <zachary.brewer.git@gmail.com>
2025-08-01 18:30:18 -0500

    More scenes

--- scripts/item_pickup.gd
+++ scripts/item_pickup.gd
@@ -5,9 +5,9 @@ class_name ItemPickup
 @export var amount: int = 1
 
 func _ready() -> void:
-    connect("body_entered", _on_body_entered)
+	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body: Node) -> void:
-    if body.has_method("add_item") and item:
-        body.add_item(item, amount)
-        queue_free()
+	if body.has_method("add_item") and item:
+		body.add_item(item, amount)
+		queue_free()
